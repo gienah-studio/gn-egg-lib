16a0f5965f764719ef2b9a3c6b002ed03c61f107
Joz <jozdortraz@gmail.com>
2020-05-23 18:06:08 +0800

    feat: update authorize function

--- gn-authorize/authorize.js
+++ gn-authorize/authorize.js
@@ -3,24 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const gn_error_1 = require("../gn-error");
 function Authorize(...authRoles) {
     const authRole = (authRoles || []).reduce((c, e) => c | e, 0);
-    return function (_target, _key, descriptor) {
+    return function (target, key, descriptor) {
         if (authRoles.length === 0) {
-            console.warn('Please provide at least one AUTHORIZE_ROLE', _target, _key);
+            console.warn('Please provide at least one AUTHORIZE_ROLE', target, key);
             return;
         }
         const originalFunction = descriptor.value;
         descriptor.value = async function (...args) {
-            const { ctx, service, userId } = this;
+            const { service, userId } = this;
             if (!userId) {
                 this.failure(new gn_error_1.default(gn_error_1.GN_ERROR_CODE.UNAUTHORIZED));
                 return;
             }
-            const { role } = await service.user.getUserInfo(userId);
+            const role = await service.core.account.getRole(userId);
             if ((role & authRole) === 0) {
                 this.failure(new gn_error_1.default(gn_error_1.GN_ERROR_CODE.UNAUTHORIZED));
                 return;
             }
-            return await originalFunction.apply(this, [...args]);
+            return await originalFunction.apply(this, args);
         };
     };
 }
--- gn-authorize/authorize.ts
+++ gn-authorize/authorize.ts
@@ -5,30 +5,30 @@ import GNError, { GN_ERROR_CODE } from '../gn-error';
 export default function Authorize(...authRoles: AUTHORIZE_ROLE[]) {
   const authRole = (authRoles || []).reduce((c, e) => c | e, 0);
 
-  return function (_target, _key, descriptor) {
+  return function (target, key, descriptor) {
     if (authRoles.length === 0) {
-      console.warn('Please provide at least one AUTHORIZE_ROLE', _target, _key);
+      console.warn('Please provide at least one AUTHORIZE_ROLE', target, key);
       return;
     }
 
     const originalFunction = descriptor.value;
 
     descriptor.value = async function (this: GNBaseController, ...args: any[]) {
-      const { ctx, service, userId } = this;
+      const { service, userId } = this;
 
       if (!userId) {
         this.failure(new GNError(GN_ERROR_CODE.UNAUTHORIZED));
         return;
       }
 
-      const { role } = await service.user.getUserInfo(userId);
+      const role = await service.core.account.getRole(userId);
 
       if ((role & authRole) === 0) {
         this.failure(new GNError(GN_ERROR_CODE.UNAUTHORIZED));
         return;
       }
 
-      return await originalFunction.apply(this, [...args]);
+      return await originalFunction.apply(this, args);
     };
   };
 }
