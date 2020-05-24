2762642aa582d4cf0dff554cf928af5f66013ee8
Joz <jozdortraz@gmail.com>
2020-05-24 18:49:29 +0800

    feat: add mobile scene code

---
+++ gn-constant/mobile.scene.js
@@ -0,0 +1,9 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+var GN_MOBILE_SCENE_CODE;
+(function (GN_MOBILE_SCENE_CODE) {
+    GN_MOBILE_SCENE_CODE["ADMIN_LOGIN"] = "10001";
+    GN_MOBILE_SCENE_CODE["BUSINESS_LOGIN"] = "20001";
+})(GN_MOBILE_SCENE_CODE || (GN_MOBILE_SCENE_CODE = {}));
+;
+exports.default = GN_MOBILE_SCENE_CODE;
---
+++ gn-constant/mobile.scene.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+enum GN_MOBILE_SCENE_CODE {
+  ADMIN_LOGIN = '10001',
+  BUSINESS_LOGIN = '20001',
+};
+
+export default GN_MOBILE_SCENE_CODE;
