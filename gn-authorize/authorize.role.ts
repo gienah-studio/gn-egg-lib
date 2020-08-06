feea98fe98212e8ec2260d16ca75c3b5ffd20194
Jozdortraz <jozdortraz@gmail.com>
2020-08-06 09:37:37 +0800

    fix: admin user role should be 1

--- gn-authorize/authorize.role.ts
+++ gn-authorize/authorize.role.ts
@@ -1,6 +1,6 @@
 import GNEnum from '../gn-enum';
 
 export default class GN_AUTHORIZE_ROLE extends GNEnum<number> {
-  public static ADMIN_USER = new GN_AUTHORIZE_ROLE(0);
-  public static INDIVIDUAL_USER = new GN_AUTHORIZE_ROLE(1 << 0);
+  public static ADMIN_USER = new GN_AUTHORIZE_ROLE(1);
+  public static NORMAL_USER = new GN_AUTHORIZE_ROLE(1 << 1);
 }
